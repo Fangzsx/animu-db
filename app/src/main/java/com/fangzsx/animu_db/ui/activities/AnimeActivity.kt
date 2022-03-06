220cc9a5bdc092619858df400ea0ec44a32eea57
jay <jygrzn@gmail.com>
2022-03-06 16:31:51 +0800

    remove utube

--- app/src/main/java/com/fangzsx/animu_db/ui/activities/AnimeActivity.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/activities/AnimeActivity.kt
@@ -1,28 +1,47 @@
 package com.fangzsx.animu_db.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TabHost
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.fangzsx.animu_db.adapters.vp.AnimeViewPagerAdapter
 import com.fangzsx.animu_db.databinding.ActivityAnimeBinding
+import com.fangzsx.animu_db.viewmodels.anime.AnimeActivityViewModel
 import com.google.android.material.tabs.TabLayoutMediator
 
 class AnimeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAnimeBinding
+    private lateinit var animeVM : AnimeActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        animeVM = ViewModelProvider(this).get(AnimeActivityViewModel::class.java)
+
+        val animeID = intent.getIntExtra("MAL_ID", 0)
+        setAnimeImage(animeID)
 
         setUpTabLayout()
+
+    }
+
+    private fun setAnimeImage(id : Int){
+        animeVM.getAnimeById(id)
+        animeVM.anime.observe(this){ animeData ->
+            Glide
+                .with(this)
+                .load(animeData.images.webp.large_image_url)
+                .into(binding.ivAnime)
+
+            binding.clToolbar.title = animeData.title
+        }
 
     }
 
     private fun setUpTabLayout() {
         val adapter = AnimeViewPagerAdapter(supportFragmentManager, lifecycle)
         binding.vpAnime.adapter = adapter
-
 
 
         TabLayoutMediator(binding.tlInfoChars, binding.vpAnime) { tab, position ->
---
+++ app/src/main/java/com/fangzsx/animu_db/viewmodels/anime/AnimeActivityViewModel.kt
@@ -0,0 +1,22 @@
+package com.fangzsx.animu_db.viewmodels.anime
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fangzsx.animu_db.models.anime.Data
+import com.fangzsx.animu_db.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
+
+class AnimeActivityViewModel : ViewModel() {
+
+    var anime : MutableLiveData<Data> = MutableLiveData()
+
+    fun getAnimeById(id : Int) = viewModelScope.launch {
+        val response = RetrofitInstance.malAPI.getAnimeByID(id)
+        if(response.isSuccessful){
+            response.body()?.let { animeData ->
+                anime.postValue(animeData.data)
+            }
+        }
+    }
+}
