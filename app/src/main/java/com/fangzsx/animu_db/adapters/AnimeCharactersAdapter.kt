9c9f9702cde3129750381bfcd64be0dd5e7ef50d
jay <jygrzn@gmail.com>
2022-03-08 13:13:59 +0800

    todo : fix info fragment follows char fragment height

---
+++ app/src/main/java/com/fangzsx/animu_db/adapters/AnimeCharactersAdapter.kt
@@ -0,0 +1,61 @@
+package com.fangzsx.animu_db.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fangzsx.animu_db.databinding.AnimeCharacterItemBinding
+import com.fangzsx.animu_db.models.animecharacter.Data
+
+class AnimeCharactersAdapter : RecyclerView.Adapter<AnimeCharactersAdapter.CharacterViewHolder>() {
+
+    inner class CharacterViewHolder(val binding : AnimeCharacterItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Data>(){
+        override fun areItemsTheSame(oldItem: Data, newItem: Data): Boolean {
+            return oldItem.character.mal_id == newItem.character.mal_id
+        }
+
+        override fun areContentsTheSame(oldItem: Data, newItem: Data): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    var differ = AsyncListDiffer(this, differCallback)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AnimeCharactersAdapter.CharacterViewHolder {
+        return CharacterViewHolder(
+            AnimeCharacterItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: AnimeCharactersAdapter.CharacterViewHolder,
+        position: Int
+    ) {
+        val character = differ.currentList[position].character
+
+        holder.binding.apply {
+            Glide
+                .with(holder.itemView)
+                .load(character.images.jpg.image_url)
+                .into(ivAnimeChar)
+
+            tvAnimeCharName.text = character.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+
+}
--- app/src/main/java/com/fangzsx/animu_db/ui/fragments/anime/viewpagerfrag/CharFragment.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/fragments/anime/viewpagerfrag/CharFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fangzsx.animu_db.R
+import com.fangzsx.animu_db.adapters.AnimeCharactersAdapter
 import com.fangzsx.animu_db.databinding.FragmentCharBinding
+import com.fangzsx.animu_db.viewmodels.anime.AnimeCharacterFragmentViewModel
 
 class CharFragment : Fragment() {
 
     private lateinit var binding : FragmentCharBinding
+    private lateinit var animeCharVM : AnimeCharacterFragmentViewModel
+    private lateinit var charactersAdapter : AnimeCharactersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        animeCharVM = AnimeCharacterFragmentViewModel()
+        charactersAdapter = AnimeCharactersAdapter()
 
     }
 
@@ -24,6 +32,22 @@ class CharFragment : Fragment() {
     ): View? {
         binding = FragmentCharBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val id = requireActivity().intent.getIntExtra("MAL_ID", 0)
+
+        animeCharVM.getCharactersByAnimeID(id)
+        animeCharVM.characters.observe(viewLifecycleOwner){ characterList ->
+            charactersAdapter.differ.submitList(characterList)
+        }
+
+        binding.rvCharacters.apply{
+            layoutManager = GridLayoutManager(activity, 3, GridLayoutManager.VERTICAL, false)
+            adapter = charactersAdapter
+        }
     }
 
 }
