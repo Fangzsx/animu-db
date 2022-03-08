47a5a2864959b75b1e2cb36e338009c13936383f
jay <jygrzn@gmail.com>
2022-03-08 12:09:15 +0800

    add binding for char anime fragment

--- app/src/main/java/com/fangzsx/animu_db/ui/fragments/anime/viewpagerfrag/CharFragment.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/fragments/anime/viewpagerfrag/CharFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.fangzsx.animu_db.R
+import com.fangzsx.animu_db.databinding.FragmentCharBinding
 
 class CharFragment : Fragment() {
 
+    private lateinit var binding : FragmentCharBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -19,8 +22,8 @@ class CharFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_char, container, false)
+        binding = FragmentCharBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/fangzsx/animu_db/viewmodels/anime/AnimeCharacterFragmentViewModel.kt
@@ -0,0 +1,24 @@
+package com.fangzsx.animu_db.viewmodels.anime
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fangzsx.animu_db.models.animecharacter.Data
+import com.fangzsx.animu_db.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
+
+class AnimeCharacterFragmentViewModel : ViewModel() {
+
+    var characters : MutableLiveData<List<Data>> = MutableLiveData()
+
+    fun getCharactersByAnimeID(id : Int) = viewModelScope.launch {
+        val response = RetrofitInstance.malAPI.getCharactersByAnimeID(id)
+        if(response.isSuccessful){
+            response.body()?.let { charactersResponse ->
+                characters.postValue(charactersResponse.data)
+            }
+        }
+    }
+
+
+}
