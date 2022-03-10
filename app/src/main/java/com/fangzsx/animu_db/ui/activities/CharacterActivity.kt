7b08542c7ca91afc9e89f4353d8d3e90dd6d506a
jay <jygrzn@gmail.com>
2022-03-10 12:10:13 +0800

    add loading animation for char activity

--- app/src/main/java/com/fangzsx/animu_db/ui/activities/CharacterActivity.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/activities/CharacterActivity.kt
@@ -2,6 +2,7 @@ package com.fangzsx.animu_db.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.fangzsx.animu_db.databinding.ActivityCharacterBinding
 import com.fangzsx.animu_db.viewmodels.anime.CharacterActivityViewModel
 
@@ -16,12 +17,19 @@ class CharacterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val id = intent.getIntExtra("CHAR_ID", 0)
-
-
         characterVM.getCharacterByID(id)
 
         characterVM.character.observe(this){ charData ->
-            binding.clToolbar.title = charData.name
+
+            binding.apply {
+                clToolbar.title = charData.name
+
+                Glide
+                    .with(this@CharacterActivity)
+                    .load(charData.images.jpg.image_url)
+                    .into(ivCharImage)
+            }
+
         }
 
 
--- app/src/main/java/com/fangzsx/animu_db/viewmodels/anime/CharacterActivityViewModel.kt
+++ app/src/main/java/com/fangzsx/animu_db/viewmodels/anime/CharacterActivityViewModel.kt
@@ -3,7 +3,6 @@ package com.fangzsx.animu_db.viewmodels.anime
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.fangzsx.animu_db.models.character.CharacterResponse
 import com.fangzsx.animu_db.models.character.Data
 import com.fangzsx.animu_db.retrofit.RetrofitInstance
 import kotlinx.coroutines.launch
