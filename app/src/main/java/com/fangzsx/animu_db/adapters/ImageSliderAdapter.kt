0892a7e04803e75c25410b7c085c62b26a7a9c14
jay <jygrzn@gmail.com>
2022-03-17 13:13:10 +0800

    show manga recommendation img

---
+++ app/src/main/java/com/fangzsx/animu_db/adapters/ImageSliderAdapter.kt
@@ -0,0 +1,47 @@
+package com.fangzsx.animu_db.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.fangzsx.animu_db.databinding.RecommendationMangaItemBinding
+import com.fangzsx.animu_db.models.recommendation.Data
+import com.smarteist.autoimageslider.SliderViewAdapter
+
+class ImageSliderAdapter : SliderViewAdapter<ImageSliderAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(val binding : RecommendationMangaItemBinding) : SliderViewAdapter.ViewHolder(binding.root)
+
+    private var list : List<Data> = mutableListOf()
+
+    fun setList(data : List<Data>){
+        list = data
+        notifyDataSetChanged()
+    }
+
+    override fun getCount(): Int {
+        return list.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?): ImageViewHolder {
+        return ImageViewHolder(
+            RecommendationMangaItemBinding.inflate(
+                LayoutInflater.from(parent?.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: ImageViewHolder?, position: Int) {
+        val recommended = list[position].entry[0]
+
+        viewHolder?.binding?.apply {
+            Glide
+                .with(viewHolder.itemView)
+                .load(recommended.images.jpg.large_image_url)
+                .into(ivRecommendedSrc)
+        }
+    }
+
+
+}
--- app/src/main/java/com/fangzsx/animu_db/ui/fragments/manga/MangaFragment.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/fragments/manga/MangaFragment.kt
@@ -7,18 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import com.fangzsx.animu_db.adapters.ImageSliderAdapter
 import com.fangzsx.animu_db.databinding.FragmentMangaBinding
 import com.fangzsx.animu_db.viewmodels.manga.MangaFragmentViewModel
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
+import com.smarteist.autoimageslider.SliderAnimations
 
 class MangaFragment : Fragment() {
 
     private lateinit var binding: FragmentMangaBinding
     private lateinit var mangaFragmentVM: MangaFragmentViewModel
+    private lateinit var imageSliderAdapter : ImageSliderAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mangaFragmentVM = ViewModelProvider(this).get(MangaFragmentViewModel::class.java)
+        imageSliderAdapter = ImageSliderAdapter()
     }
 
     override fun onCreateView(
@@ -32,15 +37,19 @@ class MangaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         mangaFragmentVM.getMangaRecommendation()
         mangaFragmentVM.recommendations.observe(viewLifecycleOwner) { list ->
             Log.i("asd", list.size.toString())
-            for(item in list){
-                Log.i("test", item.entry[0].title.toString())
-            }
 
+            imageSliderAdapter.setList(list.subList(0,10))
+            binding.isRecommendationsManga.setSliderAdapter(imageSliderAdapter)
+            binding.isRecommendationsManga.setIndicatorAnimation(IndicatorAnimationType.WORM)
+            binding.isRecommendationsManga.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+            binding.isRecommendationsManga.startAutoCycle()
         }
+
+
+
 
 
     }
