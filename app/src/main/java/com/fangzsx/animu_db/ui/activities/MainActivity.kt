f13717605841bc47ef9772b73263566dccd1730d
jay <jygrzn@gmail.com>
2022-03-24 19:08:46 +0800

    stable build

--- app/src/main/java/com/fangzsx/animu_db/ui/activities/MainActivity.kt
+++ app/src/main/java/com/fangzsx/animu_db/ui/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.fangzsx.animu_db.ui.activities
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -17,5 +20,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val navController = Navigation.findNavController(this, R.id.navHostFragment)
         NavigationUI.setupWithNavController(binding.btmNavigation, navController)
+    }
+
+    override fun onBackPressed() {
+        val alertDialog = AlertDialog.Builder(this)
+            .setCancelable(false)
+            .setTitle("Exit Application")
+            .setMessage("Are you sure you want to leave this application?")
+            .setPositiveButton("YES", DialogInterface.OnClickListener{ _, _
+                -> finish()
+            })
+            .setNegativeButton("NO", DialogInterface.OnClickListener { dialog, _
+                -> dialog.dismiss()
+            })
+
+        alertDialog.show()
     }
 }
